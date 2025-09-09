ad0864ca2de617e0b1c6787e1713d8ce78927f15
SuchithraAV <suchiav839@gmail.com>
2025-09-09 09:44:32 +0530

    Add files via upload

---
+++ backend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/api")
+def home():
+    return jsonify({"message": "Hello from Flask backend!"})
+
+if __name__ == "_main_":
+    app.run(host="0.0.0.0", port=5000)
